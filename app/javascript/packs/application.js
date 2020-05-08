62983793598794bf6fcb32e42da3356bc52d9956
Andrew Mason <andrewmcodes@protonmail.com>
2020-05-08 03:38:32 -0400

    remove sprockets

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require('@rails/ujs').start()
 require('turbolinks').start()
 require('@rails/activestorage').start()
 require('channels')
-
+require.context('../images', true, /\.(?:png|jpg|gif|ico|svg)$/)
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -26,5 +26,4 @@ import '@fortawesome/fontawesome-free/webfonts/fa-solid-900.svg'
 import '@fortawesome/fontawesome-free/webfonts/fa-solid-900.ttf'
 import '@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff'
 import '@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff2'
-import 'stylesheets/application.scss'
 import 'controllers'
